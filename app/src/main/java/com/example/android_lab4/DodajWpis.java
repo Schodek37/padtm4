8fc0c13fb77e78f15c2afa86d52a84e802af54c6
Schodek37 <schodek987@gmail.com>
2020-05-29 12:14:57 +0200

    Metody odpowiadające za wysyłanie i odbieranie danych do formularza

--- app/src/main/java/com/example/android_lab4/DodajWpis.java
+++ app/src/main/java/com/example/android_lab4/DodajWpis.java
@@ -2,13 +2,35 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class DodajWpis extends AppCompatActivity {
+public class DodajWpis extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
     }
+
+    public void wyslij(View view)
+    {
+        EditText kontrolka = (EditText) findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
+
+
+
+
+
+
+
 }
